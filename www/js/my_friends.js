cf4ddbcd3cb1d031d902efc8dc6c8c653ae5ae01
Magnus Lunde <maglunde@gmail.com>
2015-04-10 16:47:10 +0200

    viser riktig info på venner sin side og donasjonsliste

--- www/js/my_friends.js
+++ www/js/my_friends.js
@@ -1,80 +1,81 @@
-$(document).ready(function(){
+$(document).delegate("#page_my_friends","pagebeforeshow",function(){
 
 	$('img[name=add_friend]').click(function(){
 		window.location.href = "#page_add_friend";
 	});
 
 	$(document.body).on('click', 'li[name=showFriend]', function() {
-		alert(this.id);
+		localStorage.setItem("userIDtoShow", this.id);
+		window.location.href="#page_show_user_profile";
 	});
 
 	if(localStorage.getItem('userID') != null){
 		var sql = "select friendEmail, name, sum(donation.sum) as Donasjoner, picURL "
 		+"from user join friend on "
 		+"(user.email like friend.friendEmail "
-			+"and friend.userEmail like '"+localStorage.getItem('userID')+"')"
-+" left join donation on(donation.email like friend.friendEmail) group by name";
+		+"and friend.userEmail like '"+localStorage.getItem('userID')+"')"
+		+" left join donation on(donation.email like friend.friendEmail) group by name";
 
-var url = getURLappBackend();
+		var url = getURLappBackend();
 
 
-$.ajax({
-	type : "POST",
-	url : url,
-	dataType: "json",
-	data : {'getSQL' : sql},
-	success : function(response){
-		if(response.length == 0){
-			var html = '<li><a href="#" rel="external" class="show-page-loading-msg">'
-			+'<div class="li_container">'
-			+'<div class="li_left">'
-			+'<div class="circle">'
-			+'</div>'
-			+'</div>'
-			+'<div class="li_mid_left dots">'
-			+'<h3 class="li_friends">Ingen venner lagt til</h3>'	
-			+'</div>'
-			+'<div class="li_mid_right">'
-			+'</div>'
-			+'<div class="li_right">'
-			+'</div>'
-			+'</div>'
-			+'</a>'
-			+'</li>';
-			$("#friendList").append(html);
-		}else{
-
-			for(var i = 0 ; i < response.length; i++){		
-				
-				var friend='<li id="'+response[i].friendEmail+'" name="showFriend"><a href="#" rel="external" class="show-page-loading-msg">'
-				+'<div class="li_container">'
-				+'<div class="li_left">'
-				+'<div class="circle">'
-				+'<img src="'
-				+(response[i].picURL==null? '../img/no_image_avaliable.png':response[i].picURL )+'"/>'
-				+'</div>'
-				+'</div>'
-				+'<div class="li_mid_left dots">'
-				+'<span class="li_friends">'+response[i].name	+'</span>	'	
-				+'</div>'
-				+'<div class="li_mid_right donations">'
-				+'<span class="li_donations green">'
-				+(response[i].Donasjoner==null?0:response[i].Donasjoner+'')
-				+'</span>'
-				+'</div>'
-				+'<div class="li_right">'
-				+'<img src="../img/li_arrow_r_grey.png">'
-				+'</div>'
-				+'</div>'
-				+'</a>'
-				+'</li>';
-				$("#friendList").append(friend);
-			}
-		}
-	},
-	error: function(){
-		alert("error in my_friends.js error in ajax_request trying to print friend list");
+		$.ajax({
+			type : "POST",
+			url : url,
+			dataType: "json",
+			data : {'getSQL' : sql},
+			success : function(response){
+				if(response.length == 0){
+					var html = '<li><a href="#" rel="external" class="show-page-loading-msg">'
+					+'<div class="li_container">'
+					+'<div class="li_left">'
+					+'<div class="circle">'
+					+'</div>'
+					+'</div>'
+					+'<div class="li_mid_left dots">'
+					+'<h3 class="li_friends">Ingen venner lagt til</h3>'	
+					+'</div>'
+					+'<div class="li_mid_right">'
+					+'</div>'
+					+'<div class="li_right">'
+					+'</div>'
+					+'</div>'
+					+'</a>'
+					+'</li>';
+					$("#friendList").append(html);
+				}else{
+					var friends="";
+					for(var i = 0 ; i < response.length; i++){		
+						
+						friends+='<li id="'+response[i].friendEmail+'" name="showFriend"><a href="#" rel="external" class="show-page-loading-msg">'
+						+'<div class="li_container">'
+						+'<div class="li_left">'
+						+'<div class="circle">'
+						+'<img src="'
+						+(response[i].picURL==null? '../img/no_image_avaliable.png':response[i].picURL )+'"/>'
+						+'</div>'
+						+'</div>'
+						+'<div class="li_mid_left dots">'
+						+'<span class="li_friends">'+response[i].name	+'</span>	'	
+						+'</div>'
+						+'<div class="li_mid_right donations">'
+						+'<span class="li_donations green">'
+						+(response[i].Donasjoner==null?0:response[i].Donasjoner+'')
+						+'</span>'
+						+'</div>'
+						+'<div class="li_right">'
+						+'<img src="../img/li_arrow_r_grey.png">'
+						+'</div>'
+						+'</div>'
+						+'</a>'
+						+'</li>';
+						$("#friendList").html(friends);
+					}
+					}
+				},
+				error: function(){
+					alert("error in my_friends.js error in ajax_request trying to print friend list");
+				}
+		});
 	}
-});
-}
 });
--- www/js/showUserprofile.js
+++ www/js/showUserprofile.js
@@ -1,30 +1,154 @@
-$(document).ready(function(){
-	$(document.body).on('click', 'a[name=showUserprofile]', function() {
-		localStorage.setItem("userIDtoShow", this.id);
 
-		var url = getURLappBackend();
-		var data = {"getSQL" : "SELECT * FROM user WHERE email = '"+this.id+"'"};
-
-		$.ajax({
-			type : "POST",
-			url : url,
-			data : data,
-			dataType : "JSON",
-			success : function(json){
-				if(json.length == 1){
-					$('h2[name=show_profile_username]').text(json[0].email);
-					$('div[name=show_user_fullname]').text(json[0].name);
-					
-				}
-			},
-			error : function(){
-				alert("showUserprofile.js Her gikk noe galt");
-			}
-		});
-
+// First time page is loaded 
+$(document).on("pageinit","#page_show_user_profile",function(){
+	var userIDtoShow = localStorage.getItem("userIDtoShow");
+	$('a[name=show_user_donationlist]').click(function(){
+		//alert("vis: "+userIDtoShow +" sine donasjoner");
 	});
+
 	$('button[name=challenge_user]').click(function(){
 		var userID = localStorage.getItem("userIDtoShow");
-		alert(userID);
+		alert("challenge: "+userID);
 	});
-});
+}); // on pageinit
+
+// Everytime page is shown
+$(document).on("pagebeforeshow","#page_show_user_profile",function(){
+	
+	var userIDtoShow = localStorage.getItem("userIDtoShow");
+
+	var url = getURLappBackend();
+	var sql = "SELECT * FROM user WHERE email = '"+userIDtoShow+"'";
+	var data = {"getSQL" : sql};
+
+	$.ajax({
+		type : "POST",
+		url : url,
+		data : data,
+		dataType : "JSON",
+		success : function(json){
+			if(json.length == 1){
+				
+				$('span[name=show_profile_username]').text(json[0].email);
+				var picURL = json[0].picURL;
+				picURL = (picURL == null ? "../img/no_image_avaliable.png" : picURL);
+				$('img[name=user_logo]').attr("src",picURL);
+				$('span[name=show_user_fullname]').text(json[0].name);
+				localStorage.setItem("userToShowName",json[0].name);
+				getDonationInformation(userIDtoShow);
+
+				
+			}
+		},
+		error : function(){
+			alert("showUserprofile.js Her gikk noe galt");
+		}
+	}); // ajax
+}); // on pagebeforeshow
+
+
+
+function getDonationInformation(userIDtoShow){
+	var sql = "SELECT * FROM Donation WHERE email = '"+userIDtoShow+"'";
+	var url = getURLappBackend();
+	var data = {"getSQL" : sql};
+
+	$.ajax({
+		type : "POST",
+		url : url,
+		data : data,
+		dataType : "JSON",
+		success : function(json){
+			var num_donations = 0;
+			var sum_current_month = 0;
+			var current_month="";
+			var sum_total = 0;
+
+			for(var i = 0; i < json.length; i++){
+				var t = json[i].date.split(/[- :]/);
+				var date = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+
+				if(date.getMonth() == new Date().getMonth()){
+					num_donations++;
+					sum_current_month += parseInt(json[i].sum);
+				}
+				sum_total += parseInt(json[i].sum);
+			}
+			
+			var current_date = new Date();
+			var month = new Array();
+			month[0] = "Januar";
+			month[1] = "Februar";
+			month[2] = "Mars";
+			month[3] = "April";
+			month[4] = "Mai";
+			month[5] = "Juni";
+			month[6] = "Juli";
+			month[7] = "August";
+			month[8] = "September";
+			month[9] = "Oktober";
+			month[10] = "November";
+			month[11] = "Desember";
+
+			var current_month = month[current_date.getMonth()];
+
+			$('span[name=show_user_amount_current_month]').text(sum_current_month);
+			$('span[name=show_user_total_amount]').text(sum_total);
+			$('span[name=current_month]').text(current_month);
+			$('span[name=show_user_num_donations]').html(num_donations==0?"Ingen":num_donations);
+			$('span[name=is_pluar]').html(num_donations==1?"donasjon":"donasjoner");
+			
+		},// success
+		error : function(error){
+			alert("Error i showUserprofile.js getDonationInformation(userID)");
+		}
+	}); // ajax
+} // getDonationInformation(userIDtoShow)
+
+$(document).on("pagebeforeshow","#page_showUserDonations",function(){
+	// alert("page_showUserDonations");
+	listDonations(localStorage.getItem("userIDtoShow"));
+})
+
+function listDonations(userIDtoShow){
+	//alert("nå kommer\n"+userIDtoShow+"\nsine donasjoner");
+
+	$("#usernameToShow").html(localStorage.getItem("userToShowName")+"'s donasjoner");
+
+	
+	var list = $("#userDonationList");
+	var sql = "select project.name as name, donation.sum as sum, donation.type as type, donation.active as active from donation join project on (project.projectID = donation.projectID) where donation.email like '"+userIDtoShow+"'";
+	var url = getURLappBackend();
+	var data = {"getSQL" : sql};
+
+	$.ajax({
+		type: "post",
+		url: url,
+		dataType: "json",
+		data: data,
+		success:function(response){
+			var listHTML="";
+			var listItem="";
+			if(response.length == 0){
+				listItem = 	"<li>Ingen Donasjoner ennå </li>";
+				listHTML = listItem;
+				list.html(listHTML);
+			}else{
+				
+				for(var i = 0; i < response.length; i++){
+					listItem = 
+						"<li> "
+						+response[i].name+", "+response[i].sum+", "+response[i].type+", "+(response[i].type=='fast'?(response[i].active==1?"Pågår":"Avsluttet"):"");
+						+"</li>";
+					listHTML+=listItem;
+						
+				}
+				list.html(listHTML);
+			}
+
+		},
+		error:function(response){
+			alert("error: "+JSON.stringify(response.readyState));
+		}
+	});
+}
