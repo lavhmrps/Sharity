fc60bc4a3dc1abe14a91b01fb5ca230e7b249e08
Magnus Lunde <maglunde@gmail.com>
2015-04-27 04:51:12 +0200

    ny venneliste, avslå venneforespørsel, melding på skjerm om venn ble bekreftet eller avslått

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,19 +1,23 @@
-$(document).ready(function(){
-	$(".footer_settings").click(function(){
-		checkFriendRequests();
-		
-	});
+$(document).on("pagebeforeshow","#page_settings",function(){
+	$("#message").hide();	
+});
+
+$(document).on("pageinit","#page_settings",function(){
+
 	$("button[name=logout]").click(function(){
 		localStorage.clear();
 		window.location.replace("../index.html");
 	});
-	$(document).on('click', 'button[name=accept_request]', function() {
-		acceptFriendRequest(this.id);
-	});
-});
 
-$(document).on('pagebeforeshow', '#page_settings', function(){  
-	checkFriendRequests();
+	$(document).on('click', 'button[name=accept_request]', function() {
+		var from_user = $(this).closest("li").attr("id");
+		acceptFriendRequest(from_user);
+	});
+	$(document).on('click', 'button[name=deny_request]', function() {
+		var from_user = $(this).closest("li").attr("id");
+		var name = $(this).closest("li").find(".name").html();
+		denyFriendRequest(from_user,name);
+	});
 });
 
 
@@ -28,23 +32,45 @@ function checkFriendRequests(){
 		dataType: "json",
 		success : function(response){
 			var numRequests = response.length;
-			showSettingsNotif(numRequests);
-			$("#num_friend_req").text("Du har " + (numRequests==0?"ingen":numRequests) + " venneforespørs"+(numRequests==1?"el":"ler"));
-			var friend_requests = "";
+			showFriendsNotif(numRequests);
+			$("#num_friend_req").html("Du har <span class='red'>" + (numRequests==0?"ingen":numRequests) + "</span> venneforespørs"+(numRequests==1?"el":"ler"));
 
+			if ($.mobile.activePage.attr('id') != "page_settings")
+				return;
+			var friend_requests = "";
 			for(var i = 0; i < response.length; i++){
+				/*
 				friend_requests += "<li>"
-						+"<div class='li_container'><div class='li_left'><div class='circlegrey'>"
-						+(response[i].picURL == null? '':"<img src='"+response[i].picURL+"'>")+"</div>"
-						+"</div><div class='li_mid'>"
-						+ "<span class='projectName'>"+response[i].name+"</span>"
-						+"<span class='grey small block'>"+ response[i].from_user+"</span>"
-						+"</div><div class='li_right'>"
-						+"<button class='btn_deny' name='deny_request' id='"+response[i].from_user+"'>Avslå</button>"
-						+"<button class='btn_accept' name='accept_request' id='"+response[i].from_user+"'>OK</button>"
-						+ "</div></div></li>";
-					}
-					$('ul[name=friend_requests]').html(friend_requests);
+								+"<div class='li_container'><div class='li_left'><div class='circlegrey'>"
+								+(response[i].picURL == null? '':"<img src='"+response[i].picURL+"'>")+"</div>"
+								+"</div><div class='li_mid'>"
+								+ "<span class='projectName'>"+response[i].name+"</span>"
+								+"<span class='grey small block'>"+ response[i].from_user+"</span>"
+								+"</div><div class='li_right'>"
+								+"<button class='btn_deny' name='deny_request' id='"+response[i].from_user+"'>Avslå</button>"
+								+"<button class='btn_accept' name='accept_request' id='"+response[i].from_user+"'>OK</button>"
+								+ "</div></div></li>";
+				*/
+				friend_requests 	+="<li id='"+response[i].from_user+"'>"// +0
+										+"<div class='li_container'>" 	// +1
+											+"<div class='li_l'>"	// +2
+									    		+"<div class='circlegrey'>"	//+3
+											    	+(response[i].picURL == null? '':"<img src='"+response[i].picURL+"'/>")
+												+"</div>" // -3
+											+"</div>" // -2
+											+"<div class='li_r'>"// +2
+												+"<div class='li_r_top'>"// +3
+											    	+"<span class='name'>"+response[i].name+"</span>"
+											    +"</div>"// -3
+												+"<div class='li_r_bottom'>"// +3
+												  	+"<button class='small confirm' name=accept_request>BEKREFT</button>"
+												  	+"<button class='small grey dismiss' name=deny_request>AVSLÅ</button>"
+												+"</div>"// -3
+											+"</div>"// -2
+										+"</div>"// -1
+									+"</li>";// -0
+			}
+			$('ul[name=friend_requests]').html(friend_requests);
 
 		},
 		error : function(){
@@ -64,7 +90,7 @@ function acceptFriendRequest(from_user){
 		data: data,
 		dataType: "text",
 		success : function(response){
-			alert("File: settings.js : trying to accept friend request, response from SQL : " + response);
+			//alert("File: settings.js : trying to accept friend request, response from SQL : " + response);
 			if(response == "OK"){
 				var data = {"setSQL" : "DELETE FROM friend_request WHERE from_user = '"+from_user+"' AND to_user ='"+my_email+"'"};
 				$.ajax({
@@ -82,12 +108,23 @@ function acceptFriendRequest(from_user){
 	});
 }
 
-function showSettingsNotif(numRequests){
+function denyFriendRequest(from_user,name){
+	var url = getURLappBackend();
+	var my_email = localStorage['userID'];
+	var sql ="DELETE FROM friend_request WHERE from_user = '"+from_user+"' AND to_user ='"+my_email+"'"
+	$.ajax({
+		type:"POST",
+		url: url, 
+		data: {"setSQL":sql},
+		dataType: "text",
+		success : function(response){
+			console.log(response);
+			checkFriendRequests();
+		}
+	});
 	
-		$(".footer_settings").each(function(){
-			$(this).find(".notification").remove();
-			if(numRequests>0)
-				$(this).append("<div class='notification'>"+numRequests+"</div>");
-		});		
-	
+	var message="Du avslo "+name+" sin venneforespørsel.";
+	$("#messagetext").html(message).css("padding","3pt");
+	$("#messagetext").fadeIn().delay(3000).fadeOut();
 }
+
