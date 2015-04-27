391184d0c4b5f041603ead8d74639b64443d33ec
Magnus Lunde <maglunde@gmail.com>
2015-04-27 05:02:49 +0200

    'søk sharity

--- www/js/add_friends.js
+++ www/js/add_friends.js
@@ -1,67 +1,83 @@
-$(document).ready(function(){
-	$(document.body).on('click', 'button[name=add_friend]', function() {
-		requestFriendship(this.id);
-	});
-	$('li[name=search_sharity]').click(function(){
-		window.location.href='#page_search_friends_sharity';
-	});
+$(document).on("pageinit","#page_search_friends_sharity",function(){
 	var url = getURLappBackend();
-	var data = {"getSQL" : "SELECT * FROM user WHERE email !='"+localStorage['userID']+"'"};
+	var sql ="select * from friend where userEmail like '"+localStorage['userID']+"'";
+	var friends = [];
+	var pendingRequests = [];
 	$.ajax({
 		type : "POST",
-		data : data,
 		url : url,
 		dataType: "JSON",
-		success: function(response){
-			var userCode = "";
-			for(var i = 0 ; i < response.length; i++){	
-				userCode +='<li id="'+response[i].email+'">'
-							  +'<div class="li_container">'
-								+'<div class="li_left">'
-								  +'<div class="circlegrey">'
-								    +'<img src="'+(response[i].picURL == null? "../img/no_image_avaliable.png":response[i].picURL)+'"/>'
-								  +'</div>'
-								+'</div>'
-								+'<div class="li_mid ">'
-								  +'<div class="li_name">'
-								    + response[i].name
-								  +'</div>'
-								  +'<span class="li_email">'
-								    + response[i].email
-								  +'</span>'
-								+'</div>'
-								+'<div class="li_right">'
-								/*
-								  +'<div class="li_right_tophalf">'
-								    +'<a href="#page_show_user_profile" name="showUserprofile" id="'+response[i].email+'">Vis profil </a>'
-								  +'</div>'
-								*/
-								  +'<div class="li_right_center">'
-								    +'<a href="#" page-role="button" class="ui-btn" name="requestFriendship">Bli venn</a>'
-								  +'</div>'
-								+'</div>'
-							  +'</div>'
-							+'</li>';
-			}
-			$("#user_list").html(userCode);
-		}
-	});
-});
-function requestFriendship(to_user){
-	var url = getURLappBackend();
-	var from_user = localStorage['userID'];
-	var data = {"setSQL" : "INSERT INTO friend_request (from_user, to_user) VALUES('"+from_user+"', '"+to_user+"')"};
+		data:{"getSQL":sql},
+		success:function(response){
+			for(var i = 0; i < response.length; i++)
+				friends[i]=response[i].friendEmail;
 
-	$.ajax({
-		type: "POST",
-		data: data,
-		url: url,
-		dataType: "text",
-		success: function(response){
-			alert("File: add_friends.js Trying to set friend request getting response from ajax request: : " + response);
-		}
-	});
-}
+			sql ="select * from friend_request where from_user like '"+localStorage['userID']+"'";
+			$.ajax({
+				type : "POST",
+				url : url,
+				dataType: "JSON",
+				data:{"getSQL":sql},
+				success:function(response){
+					for(var i=0;i<response.length;i++){
+						pendingRequests[i]=response[i].to_user;					
+					}
+
+					var data = {"getSQL" : "SELECT * FROM user WHERE email !='"+localStorage['userID']+"'"};
+					$.ajax({
+						type : "POST",
+						data : data,
+						url : url,
+						dataType: "JSON",
+						success: function(response){
+							var userCode = "";
+							var alreadyFriend=false;
+							var requestPending=false;
+							for(var i = 0 ; i < response.length; i++){							
+								if($.inArray(response[i].email,friends) != -1)
+									alreadyFriend = true;
+								if($.inArray(response[i].email,pendingRequests) != -1){
+									requestPending = true;
+																}
+								var nameAttr = (alreadyFriend?"":"name="+(requestPending?"'cancelRequest'":"'requestFriendship'"));
+								var btnText = (alreadyFriend?"Friend":(requestPending?"Cancel":"Befriend"));
+								
+								userCode +='<li id="'+response[i].email+'">'
+											  +'<div class="li_container">'
+												+'<div class="li_left">'
+												  +'<div class="circlegrey">'
+												   +(response[i].picURL == null? "":"<img src='"+response[i].picURL+"'/>'")
+												  +'</div>'
+												+'</div>'
+												+'<div class="li_mid ">'
+												  +'<div class="li_name">'
+												    + response[i].name
+												  +'</div>'
+												  +'<span class="li_email">'
+												    + response[i].email
+												  +'</span>'
+												+'</div>'
+												+'<div class="li_right">'
+												  +'<div class="li_right_center">'
+													  +'<a href="#" page-role="button" class="addbtn" '+nameAttr+'>'+btnText+'</a>'
+												    //+'<a href="#" page-role="button" class="ui-btn" '+(alreadyFriend?'':'name="requestFriendship"')+'">'+(alreadyFriend?"Er venn":"Bli venn")+'</a>'
+												  +'</div>'
+												+'</div>'
+											  +'</div>'
+											+'</li>';
+								alreadyFriend = false;
+								requestPending = false;
+								
+							} // for
+							$("#user_list").html(userCode);
+						}// success
+					}); // ajax
+				} // success			
+			});//ajax
+		} // success
+	});// ajax
+}) 
+
 
 $(document).on("pageshow","#page_search_friends_sharity",function(){
 
@@ -72,8 +88,30 @@ $(document).on("pageshow","#page_search_friends_sharity",function(){
 	});// li left/mid on click
 
 	$("a[name=requestFriendship]").off("click").on("click",function(){
-		localStorage.setItem("userIDtoShow",$(this).closest("li").attr("id")); // id of listitem
-		alert("Venneforspørsel sendt til "+ $(this).closest("li").attr("id"));
+		var to_user = $(this).closest("li").attr("id");
+		localStorage.setItem("userIDtoShow",to_user); // id of listitem
+		requestFriendship(to_user);
+
 	}); // a on click
 
 }) // on pageshow #page_add_friend
+
+function requestFriendship(to_user){
+	var url = getURLappBackend();
+	var from_user = localStorage['userID'];
+	var sql ="INSERT INTO friend_request (from_user, to_user) VALUES('"+from_user+"', '"+to_user+"')";
+	console.log(sql);
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		dataType: "text",
+		data: {"setSQL" : sql},
+		success: function(response){
+			if(response == "OK")
+				console.log("venneforespørsel sendt til "+to_user);
+			//alert("File: add_friends.js Trying to set friend request getting response from ajax request: : " + response);
+		}
+	});
+}
+
